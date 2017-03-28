e2b5c4d10d0d47848747f2c40228d15ea88d47f7
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-03-28 17:32:24 -0600

    Improve test coverage

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -15,11 +15,13 @@ type GeneratorTestSuite struct {
 	suite.Suite
 	assert          *assert.Assertions
 	mockServiceName string
+	path            string
 }
 
 func (suite *GeneratorTestSuite) SetupSuite() {
 	suite.assert = assert.New(suite.T())
 	suite.mockServiceName = "mockService"
+	suite.path = joinPath()
 
 	GoConfig.Init(&GoConfig.ConfigOptions{
 		ConfigType: "json",
@@ -27,6 +29,7 @@ func (suite *GeneratorTestSuite) SetupSuite() {
 		ConfigPath: "..",
 	})
 	GoConfig.SetConfigValue("templatesPath", "templates/")
+	GoConfig.SetConfigValue("protosPath", "../protos/api/")
 }
 
 func (suite *GeneratorTestSuite) TearDownSuite() {
@@ -34,35 +37,64 @@ func (suite *GeneratorTestSuite) TearDownSuite() {
 }
 
 func (suite *GeneratorTestSuite) TestJoinPath() {
-	path := joinPath()
-	suite.assert.True(strings.HasPrefix(path, os.Getenv("GOPATH")))
-	suite.assert.True(strings.HasSuffix(path, "/src/github.com/reivaj05/apigateway"))
+	suite.assert.True(strings.HasPrefix(suite.path, os.Getenv("GOPATH")))
+	suite.assert.True(strings.HasSuffix(suite.path, "/src/github.com/reivaj05/apigateway"))
 }
 
 // TODO: Add unsuccessful tests
 func (suite *GeneratorTestSuite) TestGenerateAPIFileSuccessful() {
-	path := joinPath()
-	suite.assert.Nil(generateAPIFile(path, suite.mockServiceName))
+	suite.assert.Nil(generateAPIFile(suite.path, suite.mockServiceName))
 	_, err := os.Stat(fmt.Sprintf("../api/%s/%s.go",
 		suite.mockServiceName, suite.mockServiceName))
 	suite.assert.False(os.IsNotExist(err))
 }
 
 func (suite *GeneratorTestSuite) TestGenerateServiceFileSuccessful() {
-	path := joinPath()
-	suite.assert.Nil(generateServiceFile(path, suite.mockServiceName))
+	suite.assert.Nil(generateServiceFile(suite.path, suite.mockServiceName))
 	_, err := os.Stat(fmt.Sprintf("../services/%s/%s.go",
 		suite.mockServiceName, suite.mockServiceName))
 	suite.assert.False(os.IsNotExist(err))
 }
 
 func (suite *GeneratorTestSuite) TestGenerateProtoFilesSuccessful() {
-	path := joinPath()
-	suite.assert.Nil(generateProtoFiles(path, suite.mockServiceName))
+	suite.assert.Nil(generateProtoFiles(suite.path, suite.mockServiceName))
 	_, err := os.Stat(fmt.Sprintf("../protos/api/%s.proto", suite.mockServiceName))
 	suite.assert.False(os.IsNotExist(err))
 	_, err = os.Stat(fmt.Sprintf("../protos/services/%s.proto", suite.mockServiceName))
 	suite.assert.False(os.IsNotExist(err))
+}
+
+func (suite *GeneratorTestSuite) TestGetServicesNamesPopulatedList() {
+	generateAPIFile(suite.path, suite.mockServiceName)
+	services := getServicesNames()
+	suite.assert.Equal(1, len(services))
+}
+
+func (suite *GeneratorTestSuite) TestUpdateServerFilesWrongLocation() {
+	err := updateServerFiles(&EndpointsData{Services: getServicesNames()})
+	suite.assert.NotNil(err)
+}
+
+func (suite *GeneratorTestSuite) TestUpdateServerFilesGoodLocation() {
+	serverPath := GoConfig.GetConfigStringValue("serverPath")
+	GoConfig.SetConfigValue("serverPath", "../server/")
+	err := updateServerFiles(&EndpointsData{Services: getServicesNames()})
+	suite.assert.Nil(err)
+	GoConfig.SetConfigValue("serverPath", serverPath)
+}
+
+func (suite *GeneratorTestSuite) TestGenerateWithoutArgs() {
+	err := Generate()
+	suite.assert.NotNil(err)
+}
+
+func (suite *GeneratorTestSuite) TestGenerateWithArgs() {
+	serverPath := GoConfig.GetConfigStringValue("serverPath")
+	GoConfig.SetConfigValue("serverPath", "../server/")
+	GoConfig.SetConfigValue("protoGenPath", "../proto-gen.sh")
+	err := Generate(suite.mockServiceName)
+	suite.assert.Nil(err)
+	GoConfig.SetConfigValue("serverPath", serverPath)
 }
 
 func TestGenerator(t *testing.T) {
